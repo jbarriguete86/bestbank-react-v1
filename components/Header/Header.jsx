e3ef7368dfffc4ed22dce53e82e6c19094fb57c6
jbarriguete86 <104594909+jbarriguete86@users.noreply.github.com>
2024-02-03 10:48:49 -0600

    initial commit

---
+++ components/Header/Header.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import HeaderContainer from './HeaderContainer'
+import HeaderIcon from './HeaderIcon'
+import HeaderOption from './HeaderOption'
+import HamburgerIcon from './HamburgerIcon'
+import useToggle from '../../hooks/useToggle'
+import { AppContext } from '../../App'
+
+const HeaderContext = React.createContext()
+export {HeaderContext}
+
+export default function Header(){
+
+    const {selection, setSelection} = React.useContext(AppContext)
+    const [open, toggleOpen] = useToggle({
+        initialValue: false,
+    })
+
+    const headerArray = ["Home","Payments","Savings","Financing","Stocks"]
+
+    const isNotSmallDevice = window.innerWidth > 768
+
+        const [isSmallScreen, setIsSmallScreen] = React.useState(false);
+      
+        React.useEffect(() => {
+          const handleResize = () => {
+            setIsSmallScreen(window.innerWidth < 768);
+          };
+      
+          window.addEventListener('resize', handleResize);
+      
+          // Clean up the event listener
+          return () => {
+            window.removeEventListener('resize', handleResize);
+          };
+        }, []);
+
+
+    function mountComponent(){
+        const componentArr= headerArray.map(option => (
+        <HeaderOption key={option} className={selection === option && "selected"}>
+            {option}
+        </HeaderOption>))
+
+        return componentArr
+    } 
+
+    function handleClick(element){
+       setSelection(element.children)
+       !isNotSmallDevice && toggleOpen()
+      }
+
+
+    return (
+        <HeaderContext.Provider value={{handleClick, open, toggleOpen}}>
+            <header className="hero">
+                <HeaderIcon/>
+                <HamburgerIcon />
+                {(open || !isSmallScreen) && (<HeaderContainer>{mountComponent()}</HeaderContainer>)}    
+            </header>
+        </HeaderContext.Provider>
+        
+    )
+}
---
+++ components/Header/HeaderIcon.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import imageUrl from "../../assets/bestbank_logo.png"
+
+export default function HeaderIcon(){
+
+    return (
+        <img className="header-icon" src={imageUrl} alt="company logo image" />
+    )
+}
