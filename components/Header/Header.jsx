bec16b18bc855625afbecd9315968710e7c8805b
jbarriguete86 <104594909+jbarriguete86@users.noreply.github.com>
2024-02-03 11:29:42 -0600

    update for devices

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -47,7 +47,7 @@ export default function Header(){
 
     function handleClick(element){
        setSelection(element.children)
-       !isNotSmallDevice && toggleOpen()
+       isSmallScreen && toggleOpen()
       }
 
 
